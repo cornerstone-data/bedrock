228c50db8a84dd8b49ee232db7503c91361f6ced
Ben Young <Ben.Young@erg.com>
2025-11-07 17:24:49 -0500

    update test for windows path to avoid permission error

--- ceda_usa/utils/__tests__/test_gcp.py
+++ ceda_usa/utils/__tests__/test_gcp.py
@@ -6,10 +6,10 @@ from ceda_usa.utils.gcp import download_gcs_file
 
 def test_download_gcs_file() -> None:
     # TODO set up cornerstone bucket
-    tmpfile = tempfile.NamedTemporaryFile(delete=False)
+    tmpfile = './testfile.csv'
     try:
-        download_gcs_file("gs://cornerstone-default/examples/example.csv", tmpfile.name)
-        assert os.path.exists(tmpfile.name)
-        assert os.path.getsize(tmpfile.name) > 0
+        download_gcs_file("gs://cornerstone-default/examples/example.csv", tmpfile)
+        assert os.path.exists(tmpfile)
+        assert os.path.getsize(tmpfile) > 0
     finally:
-        os.unlink(tmpfile.name)
+        os.unlink(tmpfile)
