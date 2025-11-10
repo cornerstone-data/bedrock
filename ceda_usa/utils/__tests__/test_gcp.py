42a0d944c985f2b85d524737887d32d7306373bd
Ben Young <Ben.Young@erg.com>
2025-11-10 14:45:31 -0500

    update local test file windows path in

--- ceda_usa/utils/__tests__/test_gcp.py
+++ ceda_usa/utils/__tests__/test_gcp.py
@@ -1,17 +1,14 @@
 import os
-import tempfile
 
 from ceda_usa.utils.gcp import download_gcs_file
 
 
 def test_download_gcs_file() -> None:
-    fd, tmp_path = tempfile.mkstemp()
+    tmp_path = './test_file.csv'
 
-    # Close immediately so Windows can reopen it to write
-    os.close(fd)
     try:
         download_gcs_file("gs://cornerstone-default/examples/example.csv", tmp_path)
         assert os.path.exists(tmp_path)
         assert os.path.getsize(tmp_path) > 0
     finally:
-        os.unlink(tmp_path)
+        os.remove(tmp_path)
