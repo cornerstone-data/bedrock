46e31d6f6ca95869262c80d59a1f606413802b86
Ben Young <ben.young@erg.com>
2025-11-20 15:10:23 -0500

    add test for download fba

---
+++ bedrock/flowsa/__tests__/test_fba.py
@@ -0,0 +1,10 @@
+import pytest
+
+from bedrock.flowsa import getFlowByActivity
+
+
+@pytest.mark.flowsa_integration
+def test_get_fba_from_gcs() -> None:
+    df = getFlowByActivity("EPA_GHGI_T_2_1", year=2022, download_FBA_if_missing=True)
+
+    assert len(df > 0)
