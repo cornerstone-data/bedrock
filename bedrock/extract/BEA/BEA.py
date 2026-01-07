a526b4b4292ea0ffbeef015d2538a0b605c68550
Ben Young <ben.young@erg.com>
2026-01-06 21:41:28 -0500

    test equivalency of BEA datasets from prior EPA version and bedrock

--- bedrock/extract/BEA/BEA.py
+++ bedrock/extract/BEA/BEA.py
@@ -12,17 +12,21 @@ in scripts/write_BEA_data_from_useeior.py
 
 import numpy as np
 import pandas as pd
+from esupy.processed_data_mgmt import download_from_remote
 
 from bedrock.ceda_usa.extract.iot.io_2017 import (
     # _load_2017_detail_make_use_usa,
     _load_2017_detail_supply_use_usa,
     _load_usa_summary_sut,
 )
+from bedrock.ceda_usa.extract.iot.io_price_index import load_go_detail
+from bedrock.ceda_usa.transform.iot.derived_price_index import _map_detail_table
 from bedrock.extract.flowbyactivity import getFlowByActivity
 from bedrock.extract.generateflowbyactivity import generateFlowByActivity
 from bedrock.transform.flowbyfunctions import assign_fips_location_system
-from bedrock.utils.config.settings import externaldatapath
+from bedrock.utils.config.settings import PATHS
 from bedrock.utils.mapping.location import US_FIPS
+from bedrock.utils.metadata.metadata import set_fb_meta
 
 
 # %%
@@ -38,6 +42,7 @@ def bea_parse(*, source, year, **_):
         filename = 'Use_SUT_detail'
         df = _load_2017_detail_supply_use_usa(filename)
         df = df.iloc[:, 1:]  # drop first column
+        df = df.iloc[: df.index.get_loc('VAPRO') + 1]  # drop everything after last row
         df = df.reset_index()
         df = df.rename(columns={'Code': 'ActivityProducedBy'})
         # use "melt" fxn to convert colummns into rows
@@ -49,7 +54,8 @@ def bea_parse(*, source, year, **_):
     elif "Detail_Supply" in source:
         filename = 'Supply_detail'
         df = _load_2017_detail_supply_use_usa(filename)
-        df = df.iloc[1:, 1:]  # drop first row and column
+        df = df.iloc[:, 1:]  # drop first column
+        df = df.iloc[: df.index.get_loc('T017') + 1]  # drop everything after the total
         df = np.transpose(df)
         df = df.reset_index().rename(columns={'index': 'ActivityProducedBy'})
         # use "melt" fxn to convert colummns into rows
@@ -58,6 +64,9 @@ def bea_parse(*, source, year, **_):
             var_name="ActivityConsumedBy",
             value_name="FlowAmount",
         )
+
+    # TODO: Add Detail_Use_PRO_BeforeRedef
+
     elif "Summary_Supply" in source:
         filename = 'Supply_summary'
         df = _load_usa_summary_sut(filename, year)
@@ -82,14 +91,22 @@ def bea_parse(*, source, year, **_):
             var_name="ActivityConsumedBy",
             value_name="FlowAmount",
         )
+
+    # TODO: add Summary_Use_PRO_BeforeRedef
+    # TODO: add Summary_Make_BeforeRedef
     elif "GrossOutput" in source:
-        filename = f'{source}_17sch'
-        df = pd.read_csv(externaldatapath / f"{filename}.csv")
-        df = df.rename(columns={'Unnamed: 0': 'ActivityProducedBy'})
-        df = df.melt(
-            id_vars=["ActivityProducedBy"], var_name="Year", value_name="FlowAmount"
+        df = _map_detail_table(load_go_detail())
+        df = df.iloc[:, 1:]  # drop first column
+        df = df.rename(columns={'sector_code': 'ActivityProducedBy'})
+        df = (
+            df.melt(
+                id_vars=["ActivityProducedBy"], var_name="Year", value_name="FlowAmount"
+            )
+            .groupby(['ActivityProducedBy', 'Year'])['FlowAmount']
+            .sum()
+            .reset_index()
         )
-        df = df[df['Year'] == year]
+        df = df[df['Year'] == str(year)]
     else:
         raise KeyError(f'{source} not available')
 
@@ -107,17 +124,63 @@ def bea_parse(*, source, year, **_):
     df["Unit"] = "Million USD"
     df['DataReliability'] = 5  # tmp
     df['DataCollection'] = 5  # tmp
-    df['Description'] = filename
+    df['Description'] = (
+        f'{source}_17sch' if 'GrossOutput' in source else f'{source}_{year}_17sch'
+    )
+
+    # Trim all string columns but avoid errors
+    obj = df.select_dtypes(include="object")
+    df[obj.columns] = obj.apply(
+        lambda s: s.map(lambda x: x.strip() if isinstance(x, str) else x)
+    )
 
     return df
 
 
+# %%
 if __name__ == "__main__":
-    for y in range(2022, 2023):
-        generateFlowByActivity(year=y, source='BEA_Summary_Supply')
-        generateFlowByActivity(year=y, source='BEA_Summary_Use_SUT')
-        fba = getFlowByActivity('BEA_Summary_Supply', y)
-        fba2 = getFlowByActivity('BEA_Summary_Use_SUT', y)
-    generateFlowByActivity(year=2017, source="BEA_Detail_Use_SUT")
-    generateFlowByActivity(year=2017, source="BEA_Detail_Supply")
-    generateFlowByActivity(year=2018, source="BEA_Detail_GrossOutput_IO")
+
+    methods = [
+        'BEA_Detail_Supply',  # Success
+        'BEA_Detail_GrossOutput_IO',  # Fails due to flow amount
+        'BEA_Detail_Use_SUT',  # Success
+        # 'BEA_Detail_Use_PRO_BeforeRedef',
+        'BEA_Summary_Supply',  # Fails due to flow amount - GCS has older data
+        'BEA_Summary_Use_SUT',  # Fails due to flow amount - GCS has older data
+        # 'BEA_Summary_Make_BeforeRedef'
+        # 'BEA_Summary_Use_PRO_BeforeRedef',
+    ]
+    ## COMPARISON requires that the newly generated FBA does not yet exist or it
+    ## will pull itself and compare to itself
+
+    ## see flowsa PR https://github.com/USEPA/flowsa/pull/456/changes where
+    # summary data were updated for v2.1
+
+    for method in methods:
+        print(f'\n\n{method}')
+        # method = 'BEA_Detail_Supply'
+        # year = 2017
+        year = 2022 if any(st in method for st in ['Summary', 'Gross']) else 2017
+        f = set_fb_meta(f'{method}_{year}', 'FlowByActivity')
+        download_from_remote(f, PATHS)
+        fba1 = getFlowByActivity(method, year)
+        generateFlowByActivity(year=year, source=method)
+        fba2 = getFlowByActivity(method, year)
+
+        set(fba1['ActivityProducedBy']).difference(set(fba2['ActivityProducedBy']))
+        set(fba2['ActivityProducedBy']).difference(set(fba1['ActivityProducedBy']))
+
+        set(fba1['ActivityConsumedBy']).difference(set(fba2['ActivityConsumedBy']))
+        set(fba2['ActivityConsumedBy']).difference(set(fba1['ActivityConsumedBy']))
+        if 'Summary' in method or 'GrossOutput' in method:
+            pd.testing.assert_frame_equal(
+                fba1.drop(columns='FlowAmount'), fba2.drop(columns='FlowAmount')
+            )
+        else:
+            pd.testing.assert_frame_equal(fba1, fba2)
+
+    # for y in range(2022, 2023):
+    #     generateFlowByActivity(year=y, source='BEA_Summary_Supply')
+    #     generateFlowByActivity(year=y, source='BEA_Summary_Use_SUT')
+    #     fba = getFlowByActivity('BEA_Summary_Supply', y)
+    #     fba2 = getFlowByActivity('BEA_Summary_Use_SUT', y)
