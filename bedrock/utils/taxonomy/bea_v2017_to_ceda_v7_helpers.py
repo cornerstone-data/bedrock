ae618383d6091fcd212186651acf8f3d7613cfe0
Mo Li <mo@watershedclimate.com>
2026-02-17 09:13:03 -0800

    fix

--- bedrock/utils/taxonomy/bea_v2017_to_ceda_v7_helpers.py
+++ bedrock/utils/taxonomy/bea_v2017_to_ceda_v7_helpers.py
@@ -5,6 +5,9 @@ from bedrock.utils.taxonomy.bea.v2012_summary import BEA_2012_SUMMARY_CODES
 from bedrock.utils.taxonomy.bea.v2017_commodity_summary import (
     BEA_2017_COMMODITY_SUMMARY_CODE,
 )
+from bedrock.utils.taxonomy.bea.v2017_industry_summary import (
+    USA_2017_SUMMARY_INDUSTRY_CODES,
+)
 from bedrock.utils.taxonomy.cornerstone.commodities import COMMODITIES
 from bedrock.utils.taxonomy.correspondence import create_correspondence_matrix
 from bedrock.utils.taxonomy.mappings.bea_v2017_commodity__bea_ceda_v7 import (
@@ -19,9 +22,9 @@ from bedrock.utils.taxonomy.mappings.bea_v2017_commodity__cornerstone_commodity 
 from bedrock.utils.taxonomy.utils import reverse, traverse
 
 
-def load_bea_v2017_summary_to_ceda_v7() -> dict[
-    BEA_2017_COMMODITY_SUMMARY_CODE, list[CEDA_V7_SECTOR]
-]:
+def load_bea_v2017_summary_to_ceda_v7() -> (
+    dict[BEA_2017_COMMODITY_SUMMARY_CODE, list[CEDA_V7_SECTOR]]
+):
     commodity_to_ceda_v7 = load_bea_v2017_commodity_to_bea_ceda_v7()
     commodity_to_summary = load_bea_v2017_commodity_to_bea_v2017_summary()
 
@@ -32,9 +35,9 @@ def load_bea_v2017_summary_to_ceda_v7() -> dict[
     return reverse(ceda_v7_to_summary, new_domain=set(BEA_2012_SUMMARY_CODES))
 
 
-def load_bea_v2017_summary_to_cornerstone() -> dict[
-    BEA_2017_COMMODITY_SUMMARY_CODE, list[str]
-]:
+def load_bea_v2017_summary_to_cornerstone() -> (
+    dict[BEA_2017_COMMODITY_SUMMARY_CODE, list[str]]
+):
     commodity_to_cornerstone = load_bea_v2017_commodity_to_cornerstone_commodity()
     commodity_to_summary = load_bea_v2017_commodity_to_bea_v2017_summary()
 
@@ -59,7 +62,9 @@ def get_bea_v2017_summary_to_ceda_corresp_df() -> pd.DataFrame:
 def get_bea_v2017_summary_to_cornerstone_corresp_df() -> pd.DataFrame:
     summary_to_cornerstone = load_bea_v2017_summary_to_cornerstone()
     corresp_df = create_correspondence_matrix(summary_to_cornerstone).reindex(
-        index=COMMODITIES, fill_value=0
+        index=COMMODITIES,
+        columns=USA_2017_SUMMARY_INDUSTRY_CODES,
+        fill_value=0,
     )
     corresp_df.index.name = 'sector'
     return corresp_df
