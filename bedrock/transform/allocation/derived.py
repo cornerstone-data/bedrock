ba37071a9f3882e205db7b5ccbde3bd175653804
Ben Young (ERG) <44471635+bl-young@users.noreply.github.com>
2026-03-11 12:35:26 -0400

    cleans up configs (#263)
    * drops the `hybrid_bea_naics_schmea_in_ghg_attribution` flag which is mostly superfluous, use the Cornerstone target schema directly; simplify ng_petrol flag name and clean up and clarify the configs
    
    * fix error

--- bedrock/transform/allocation/derived.py
+++ bedrock/transform/allocation/derived.py
@@ -130,9 +130,7 @@ def load_E_from_flowsa() -> pd.DataFrame:
     - GHG_national_Cornerstone_2023 when
       new_ghg_method is True
     - GHG_national_Cornerstone_2023_petroleum_natgas when
-      update_ghg_attribution_method_for_electricity_soda_ash_and_ng_and_petrol_systems is True
-    - GHG_national_Cornerstone_2023_hybrid_bea_naics_schema when
-      hybrid_bea_naics_schema_in_ghg_attribution is True
+      update_ghg_attribution_method_for_ng_and_petrol_systems is True
     - GHG_national_CEDA_2023 otherwise
 
     Only used when load_E_from_flowsa is True in USA config.
@@ -140,12 +138,8 @@ def load_E_from_flowsa() -> pd.DataFrame:
     usa = get_usa_config()
     if usa.new_ghg_method:
         methodname = 'GHG_national_Cornerstone_2023'
-    elif (
-        usa.update_ghg_attribution_method_for_electricity_soda_ash_and_ng_and_petrol_systems
-    ):
+    elif usa.update_ghg_attribution_method_for_ng_and_petrol_systems:
         methodname = 'GHG_national_Cornerstone_2023_petroleum_natgas'
-    elif usa.hybrid_bea_naics_schema_in_ghg_attribution:
-        methodname = 'GHG_national_Cornerstone_2023_update_schema'
     else:
         methodname = 'GHG_national_CEDA_2023'
     fbs = getFlowBySector(methodname=methodname)
--- bedrock/utils/config/usa_config.py
+++ bedrock/utils/config/usa_config.py
@@ -47,10 +47,9 @@ class USAConfig(BaseModel):
     load_E_from_flowsa: bool = False  # if True, use load_E_from_flowsa()
     usa_ghg_methodology: ta.Literal["national", "state"] = "national"
     update_transportation_ghg_method: bool = False  # DRI: ben.young
-    update_ghg_attribution_method_for_electricity_soda_ash_and_ng_and_petrol_systems: (
-        bool
-    ) = False  # DRI: catherine.birney
-    hybrid_bea_naics_schema_in_ghg_attribution: bool = False  # DRI: ben.young
+    update_ghg_attribution_method_for_ng_and_petrol_systems: bool = (
+        False  # DRI: catherine.birney
+    )
     new_ghg_method: bool = False  # if True, it is the new Cornerstone GHG FBS
 
     #####
