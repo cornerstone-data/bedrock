1f8704b320ec32dd578d8a7b2bb20eaa7caf9425
catherinebirney <60186515+catherinebirney@users.noreply.github.com>
2026-03-11 13:18:37 -0600

    drop outdated config

--- bedrock/utils/config/usa_config.py
+++ bedrock/utils/config/usa_config.py
@@ -53,7 +53,6 @@ class USAConfig(BaseModel):
     )
     new_ghg_method: bool = False  # if True, it is the new Cornerstone GHG FBS
     add_new_ghg_activities: bool = False  # DRI: catherine.birney
-    hybrid_bea_naics_schema_in_ghg_attribution: bool = False  # DRI: ben.young
 
     #####
     # Baseline snapshot
