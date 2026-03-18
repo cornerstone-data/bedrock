0014d3e917ae0e92b578c64a68931b9f8630baf0
catherinebirney <60186515+catherinebirney@users.noreply.github.com>
2026-03-18 16:28:20 -0700

    address api key error on fbs runs

--- bedrock/transform/conftest.py
+++ bedrock/transform/conftest.py
@@ -6,6 +6,15 @@ import pytest
 from bedrock.utils.snapshots.loader import load_current_snapshot
 
 
+@pytest.fixture(autouse=True)
+def download_fba_on_api_error_for_eeio_tests(request: pytest.FixtureRequest) -> None:
+    """When running eeio_integration tests, download FBA from GCS if API key is missing."""
+    if request.node.get_closest_marker("eeio_integration"):
+        import bedrock.utils.config.common as common
+
+        common.download_fba_on_api_error = True
+
+
 # snapshots
 @pytest.fixture(scope="session")
 def E_usa_es_snapshot() -> pd.DataFrame:
