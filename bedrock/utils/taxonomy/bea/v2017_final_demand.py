6c8d3305fed75269f160a980eb734d941dbee372
catherinebirney <60186515+catherinebirney@users.noreply.github.com>
2026-04-17 12:17:08 -0600

    comment file paths

--- bedrock/utils/taxonomy/bea/v2017_final_demand.py
+++ bedrock/utils/taxonomy/bea/v2017_final_demand.py
@@ -1,7 +1,7 @@
 import typing as ta
 
 USA_2017_FINAL_DEMAND_DESC = {
-    # from GCS_CEDA_INPUT_DIR/USA_AllTablesSUP/Use_SUT_Framework_2017_DET.xlsx
+    # from GCS_EXTRACT_INPUT_DIR/USA-AllTablesSUP/Use_SUT_Framework_2017_DET.xlsx
     # order matters!
     "F01000": "Personal consumption expenditures",
     "F02E00": "Nonresidential private fixed investment in equipment",
