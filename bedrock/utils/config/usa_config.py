9246469051953c6f3ef1462327a6096646349f09
WesIngwersen <ingwersw@stanford.edu>
2026-05-28 15:34:10 -0400

    Simplify margin flag names.; add new approach for margins as cornerstone_margins including filters and a new negative handling logic

--- bedrock/utils/config/usa_config.py
+++ bedrock/utils/config/usa_config.py
@@ -83,8 +83,9 @@ class USAConfig(BaseModel):
     scale_a_matrix_with_commodity_price_index: bool = False  # DRI: mo.li
     adjust_summary_A_and_q_dollar_year: bool = False  # DRI: mo.li
     apply_inflation_to_V: bool = False  # DRI: WesIngwersen
-    apply_ceda_margins_filters: bool = False  # DRI: WesIngwersen
-    apply_useeio_margins_filters: bool = False  # DRI: WesIngwersen
+    ceda_margins: bool = False  # DRI: WesIngwersen
+    useeio_margins: bool = False  # DRI: WesIngwersen
+    cornerstone_margins: bool = False  # DRI: WesIngwersen
     ### GHG Methodology selection
     load_E_from_flowsa: bool = False  # if True, use load_E_from_flowsa()
     usa_ghg_methodology: ta.Literal['national', 'state'] = 'national'
